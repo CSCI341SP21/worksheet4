7b9508a7483f59666482afaf6e3d37be832caa51
Cynthia Taylor <ctaylor@oberlin.edu>
2021-02-15 13:34:53 -0500

    updated make file, fixed warnings

--- Makefile
+++ Makefile
@@ -1,15 +1,17 @@
 LIBS = -L/usr/local/lib
+CC:= gcc
+CFLAGS := -Wall
 
 all: gdb strace ltrace
 
 gdb: gdb.c
-	gcc -g gdb.c -o trygdb
+	$(CC) -g gdb.c -o trygdb
 
 strace: strace.c
-	gcc -g strace.c -o trystrace
+	$(CC) -g strace.c -o trystrace
 
 ltrace: ltrace.c
-	gcc -g ltrace.c -o tryltrace
+	$(CC) -g ltrace.c -o tryltrace
 
 clean:
 	rm trygdb trystrace tryltrace
--- ltrace.c
+++ ltrace.c
@@ -7,12 +7,12 @@ const char* str3 = "3";
 
 int main(){
 
-  printf("%s is %d chars long\n", str2, strlen(str2));
+  printf("%s is %lu chars long\n", str2, strlen(str2));
   int i = 0;
   for (i=0; i<5;i++){
     strncat(str1, str2, 9);
   }
-  printf("%s is %d chars long\n", str1, strlen(str1));
+  printf("%s is %lu chars long\n", str1, strlen(str1));
   printf("%s\n", strpbrk(str2, str3));
   
  
